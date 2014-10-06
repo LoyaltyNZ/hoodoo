27b1d7155daacec648375c55c03ebbab38a7952e
Tom Cully <tom.cully@loyalty.co.nz>
2014-10-06 16:55:44 +1300

    Removed redundant BaseService and examples

--- lib/api_tools.rb
+++ lib/api_tools.rb
@@ -18,7 +18,6 @@ require root+'services/http_response'
 require root+'services/amqp_endpoint'
 require root+'services/amqp_multithreaded_endpoint'
 
-require root+'services/base_service'
 require root+'services/base_multithreaded_service'
 require root+'services/base_client'
 
