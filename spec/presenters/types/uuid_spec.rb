65b48d6406c766d551f230bfaef9c9873486218a
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2014-10-31 10:07:23 +1300

    Move Tags and UUID types from Documented layer to base Presenters layer, so they're more generic. Update all doc references etc. and tests.

---
+++ spec/presenters/types/uuid_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe ApiTools::Presenters::UUID do
+
+  before do
+    @inst = ApiTools::Presenters::Tags.new('one',:required => false)
+  end
+
+  describe '#validate' do
+    it 'should return no errors when valid UUID' do
+      expect(ApiTools::Presenters::UUID.new('one').validate(ApiTools::UUID.generate())).to eq([])
+    end
+
+    it 'should return correct error when data is not a UUID-like string' do
+      errors = ApiTools::Presenters::UUID.new('one').validate('1234')
+
+      err = [  {'code'=>"generic.invalid_string", 'message'=>"UUID `one` is of incorrect length `4` (should be `32`)", 'reference'=>"one"}]
+      expect(errors).to eq(err)
+    end
+
+    it 'should return correct error when data is not even a string' do
+      errors = ApiTools::Presenters::UUID.new('one').validate(1234)
+
+      err = [  {'code'=>"generic.invalid_string", 'message'=>"UUID `one` is invalid", 'reference'=>"one"}]
+      expect(errors).to eq(err)
+    end
+  end
+end
