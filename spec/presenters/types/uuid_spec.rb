6e65edd282c6e24c458bdfe10052136a71709795
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2014-11-13 10:54:14 +1300

    Improved test coverage for UUID refactor.

--- spec/presenters/types/uuid_spec.rb
+++ spec/presenters/types/uuid_spec.rb
@@ -6,15 +6,36 @@ describe ApiTools::Presenters::UUID do
       expect(ApiTools::Presenters::UUID.new('one').validate(ApiTools::UUID.generate()).errors).to eq([])
     end
 
-    it 'should return correct error when data is not a UUID-like string' do
+    it 'should return correct error when data is not a UUID-like string (1)' do
       errors = ApiTools::Presenters::UUID.new('one').validate('1234')
 
       err = [  {'code'=>"generic.invalid_uuid", 'message'=>"Field `one` is an invalid UUID", 'reference'=>"one"}]
       expect(errors.errors).to eq(err)
     end
 
-    it 'should return correct error when data is not even a string' do
+    it 'should return correct error when data is not a UUID-like string (2)' do
+      errors = ApiTools::Presenters::UUID.new('one').validate('01234567890123456789012345678901z')
+
+      err = [  {'code'=>"generic.invalid_uuid", 'message'=>"Field `one` is an invalid UUID", 'reference'=>"one"}]
+      expect(errors.errors).to eq(err)
+    end
+
+    it 'should return correct error when data is not a UUID-like string (3)' do
+      errors = ApiTools::Presenters::UUID.new('one').validate('3ba258c0-4ce3-0132-9862-28373700b71c')
+
+      err = [  {'code'=>"generic.invalid_uuid", 'message'=>"Field `one` is an invalid UUID", 'reference'=>"one"}]
+      expect(errors.errors).to eq(err)
+    end
+
+    it 'should return correct error when data is not even a string (1)' do
       errors = ApiTools::Presenters::UUID.new('one').validate(1234)
+
+      err = [  {'code'=>"generic.invalid_uuid", 'message'=>"Field `one` is an invalid UUID", 'reference'=>"one"}]
+      expect(errors.errors).to eq(err)
+    end
+
+    it 'should return correct error when data is not even a string (2)' do
+      errors = ApiTools::Presenters::UUID.new('one').validate(ApiTools::UUID.generate().to_sym)
 
       err = [  {'code'=>"generic.invalid_uuid", 'message'=>"Field `one` is an invalid UUID", 'reference'=>"one"}]
       expect(errors.errors).to eq(err)
