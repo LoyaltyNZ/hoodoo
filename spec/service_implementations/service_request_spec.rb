b51c745a37ceb419226900dbaf027872b8c726a9
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2014-11-13 11:07:16 +1300

    To make life easier for service authors, have the search, filter, reference and embed data inside a ServiceRequest object default to an empty object or array rather than nil.

--- spec/service_implementations/service_request_spec.rb
+++ spec/service_implementations/service_request_spec.rb
@@ -29,5 +29,21 @@ describe ApiTools::ServiceRequest do
       expect(@r.uri_path_components).to be_nil
       expect(@r.ident).to be_nil
     end
+
+    it 'has correct default values' do
+      expect( @r.locale              ).to eq( 'en-nz'      )
+      expect( @r.body                ).to eq( nil          )
+      expect( @r.uri_path_components ).to eq( []           )
+      expect( @r.ident               ).to eq( nil          )
+      expect( @r.uri_path_extension  ).to eq( ''           )
+      expect( @r.list_offset         ).to eq( 0            )
+      expect( @r.list_limit          ).to eq( 50           )
+      expect( @r.list_sort_key       ).to eq( 'created_at' )
+      expect( @r.list_sort_direction ).to eq( 'desc'       )
+      expect( @r.list_search_data    ).to eq( {}           )
+      expect( @r.list_filter_data    ).to eq( {}           )
+      expect( @r.embeds              ).to eq( []           )
+      expect( @r.references          ).to eq( []           )
+    end
   end
 end
