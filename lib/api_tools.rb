b69da760a36b57c34eed050370da655a0a63b360
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2014-11-17 12:45:58 +1300

    Remove ThreadSafeHash. It's no longer used or necessary for ApiTools.

--- lib/api_tools.rb
+++ lib/api_tools.rb
@@ -1,12 +1,10 @@
 root = File.dirname(__FILE__)+'/api_tools/'
 require root+'uuid'
-require root+'thread_safe'
 require root+'logger'
 require root+'platform_errors'
 require root+'json_errors'
 require root+'json_payload'
 require root+'platform_context'
-require root+'thread_safe'
 require root+'utilities.rb'
 
 require root+'presenters/base_dsl.rb'
