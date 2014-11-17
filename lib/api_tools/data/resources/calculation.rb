b0d18a0bf88d4fe845eae2f08cc5f0a29f9c6b16
David Mitchell <balance.it@gmail.com>
2014-11-18 09:45:32 +1300

    adding the calculation resource

---
+++ lib/api_tools/data/resources/calculation.rb
@@ -0,0 +1,29 @@
+########################################################################
+# File::    balance.rb
+# (C)::     Loyalty New Zealand 2014
+#
+# Purpose:: Define documented Platform API Resource 'Calculation'.
+# ----------------------------------------------------------------------
+#           17-Nov-2014 (DAM): Created.
+########################################################################
+
+module ApiTools
+  module Data
+    module Resources
+
+      # Documented Platform API Resource 'Calculation'.
+      #
+      class Calculation < ApiTools::Data::DocumentedPresenter
+
+        schema do
+          object :configuration,   :required => true do
+            type :CalculatorConfiguration
+          end
+          array :currency_amounts, :required => true do
+            type :CurrencyAmount
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/data/resources/calculation_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ApiTools::Data::Resources::Calculation do
+  it 'should match schema expectations' do
+    schema = described_class.get_schema()
+
+    # internationalised as the voucher earner is.
+    expect(schema.is_internationalised?()).to eq(true)
+
+    expect(schema.properties.count).to eq(2)
+
+    expect(schema.properties['configuration']).to be_a(ApiTools::Presenters::Object)
+    expect(schema.properties['currency_amounts']).to be_a(ApiTools::Presenters::Array)
+  end
+end
