9cc08b442b852b8a6d67b43d5348f471215755cd
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2014-11-25 16:09:14 +1300

    Lots of fixes to prototype 'Finder' code.

--- lib/api_tools/active_record/finder.rb
+++ lib/api_tools/active_record/finder.rb
@@ -20,54 +20,65 @@ module ApiTools
     #
     module Finder
 
-      def self.polymorphic_find( finder, ident )
-        id_fields  = [ :id ]
-        id_fields += @@_api_tools_show_id_fields unless @@_api_tools_show_id_fields.nil?
+      def self.included( model )
+        model.extend( ClassMethods )
+      end
 
-        id_fields.each do | field |
-          checker = finder.where( field => ident )
-          return checker.first unless checker.count == 0
+      module ClassMethods
+
+        def polymorphic_find( finder, ident )
+          extra_fields = class_variable_get( '@@_api_tools_show_id_fields' ) || []
+          id_fields    = [ :id ] + extra_fields
+
+          id_fields.each do | field |
+            checker = finder.where( field => ident )
+            return checker.first unless checker.count == 0
+          end
+
+          return nil
         end
 
-        return nil
-      end
+        def polymorphic_id_fields( *args )
+          class_variable_set( '@@_api_tools_show_id_fields', args )
+        end
 
-      def self.polymorphic_id_fields( array )
-        @@_api_tools_show_id_fields = array
-      end
+        def list_finder( context )
+          finder = self
+          finder = finder.offset( context.request.list_offset ).limit( context.request.list_limit )
+          finder = finder.order( { context.request.list_sort_key => context.request.list_sort_direction.to_sym } )
 
-      def self.list_finder( context )
-        finder = self
-        finder = finder.offset( context.request.list_offset ).limit( context.request.list_limit )
-        finder = finder.order( { context.request.list_sort_key => context.request.list_sort_direction.to_sym } )
+          search_map = class_variable_get( '@@_api_tools_list_search_map' )
 
-        unless @@_api_tools_list_search_data.nil?
-          @@_api_tools_list_search_data.each do | attr, args_for_where |
-            value = context.request.list_search_data[ attr ]
-            next if value.nil?
+          unless search_map.nil?
+            search_map.each do | attr, args_for_where |
+              value = context.request.list_search_data[ attr ]
+              next if value.nil?
 
-            args_for_where ||= [ { attr => value } ]
-            finder = finder.where( *args_for_where )
+              args_for_where ||= [ { attr => value } ]
+              finder = finder.where( *args_for_where )
+            end
+          end
+
+          filter_map = class_variable_get( '@@_api_tools_list_filter_map' )
+
+          unless filter_map.nil?
+            filter_map.each do | attr, args_for_where_not |
+              value = context.request.list_filter_data[ attr ]
+              next if value.nil?
+
+              args_for_where ||= [ { attr => value } ]
+              finder = finder.where.not( *args_for_where_not )
+            end
           end
         end
 
-        unless @@_api_tools_list_filter_data.nil?
-          @@_api_tools_list_filter_data.each do | attr, args_for_where_not |
-            value = context.request.list_filter_data[ attr ]
-            next if value.nil?
-
-            args_for_where ||= [ { attr => value } ]
-            finder = finder.where.not( *args_for_where_not )
-          end
+        def list_search_map( map )
+          class_variable_set( '@@_api_tools_list_search_map', map )
         end
-      end
 
-      def self.list_search_map( data )
-        @@_api_tools_list_search_data = data
-      end
-
-      def self.list_filter_map( data )
-        @@_api_tools_list_filter_data = data
+        def list_filter_map( map )
+          class_variable_set( '@@_api_tools_list_filter_map', map )
+        end
       end
     end
   end
