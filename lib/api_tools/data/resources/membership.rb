04d725c24645a466e73df73286e8a2ebe63055c4
thelollies <rorystephenson@gmail.com>
2014-12-03 13:26:39 +1300

    Add membership resource schema

---
+++ lib/api_tools/data/resources/membership.rb
@@ -0,0 +1,27 @@
+########################################################################
+# File::    membership.rb
+# (C)::     Loyalty New Zealand 2014
+#
+# Purpose:: Define documented Platform API Resource 'Token'.
+# ----------------------------------------------------------------------
+#           03-Dec-2014 (RJS): Created.
+########################################################################
+
+module ApiTools
+  module Data
+    module Resources
+
+      # Documented Platform API Resource 'Membership'.
+      #
+      class Membership < ApiTools::Data::DocumentedPresenter
+
+        schema do
+          text :token_identifier, required: true
+          uuid :programme_id,     required: true, resource: :Programme
+          type :CalculatorConfiguration
+        end
+
+      end
+    end
+  end
+end
