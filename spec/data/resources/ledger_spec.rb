d91571baa5dde6ca99c3c368ee2225c5463d6e03
David Mitchell <balance.it@gmail.com>
2015-01-08 14:59:57 +1300

    define the ledger resource

---
+++ spec/data/resources/ledger_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe ApiTools::Data::Resources::Ledger do
+  it 'should match schema expectations' do
+    schema = described_class.get_schema()
+
+    expect(schema.is_internationalised?()).to eq(false)
+    expect(schema.properties.count).to eq(6)
+
+    expect(schema.properties['token_identifier']).to be_a(ApiTools::Presenters::Text)
+    expect(schema.properties['token_identifier'].required).to eq(true)
+
+    expect(schema.properties['participant_id']).to be_a(ApiTools::Presenters::UUID)
+    expect(schema.properties['participant_id'].resource).to eq(:Participant)
+    expect(schema.properties['participant_id'].required).to eq(true)
+
+    expect(schema.properties['outlet_id']).to be_a(ApiTools::Presenters::UUID)
+    expect(schema.properties['outlet_id'].resource).to eq(:Outlet)
+    expect(schema.properties['outlet_id'].required).to eq(true)
+
+    expect(schema.properties['reason']).to be_a(ApiTools::Presenters::Enum)
+    expect(schema.properties['reason'].from).to eq(['calculation', 'manipulation'])
+    expect(schema.properties['reason'].required).to eq(true)
+
+    expect(schema.properties['debit']).to be_a(ApiTools::Presenters::Object)
+    expect(schema.properties['debit'].required).to eq(false)
+
+    expect(schema.properties['credit']).to be_a(ApiTools::Presenters::Object)
+    expect(schema.properties['credit'].required).to eq(false)
+  end
+end
