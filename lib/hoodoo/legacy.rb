08b5f8e955377fc5a8b518bd4b3347163cc76dd9
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2015-01-26 17:27:07 +1300

    Add (currently hypothetical, in terms of current implementation) mapping from ApiTools to Hoodoo.

---
+++ lib/hoodoo/legacy.rb
@@ -0,0 +1,11 @@
+########################################################################
+# File::    legacy.rb
+# (C)::     Loyalty New Zealand 2014
+#
+# Purpose:: Map from old to new. This will be deleted in due course.
+# ----------------------------------------------------------------------
+#           26-Jan-2015 (ADH): Created.
+########################################################################
+
+ApiTools                    = Hoodoo
+ApiTools::ServiceMiddleware = Hoodoo::Services::Middleware
