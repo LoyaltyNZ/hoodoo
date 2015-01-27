724d3456c3638e33ff8ab4c71511367e9d0f749e
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2015-01-27 16:23:00 +1300

    Back to 100% test coverage (including the new, unused Permissions code).

--- spec/services/services/permissions_spec.rb
+++ spec/services/services/permissions_spec.rb
@@ -92,27 +92,78 @@ describe Hoodoo::Services::Permissions do
     })
   end
 
-  it 'loads from a Hash' do
-    p1 = described_class.new
-    p2 = described_class.new
+  context 'with permissions set' do
+    before :each do
+      @p = described_class.new
 
-    p2.set_default_fallback( described_class::ALLOW )
+      @p.set_default_fallback( described_class::DENY )
 
-    p2.set_default( 'create', described_class::ALLOW )
-    p2.set_default( 'update', described_class::ASK   )
-    p2.set_default( 'delete', described_class::DENY  )
+      @p.set_default( 'create', described_class::ASK   )
+      @p.set_default( 'update', described_class::DENY  )
+      @p.set_default( 'delete', described_class::ALLOW )
 
-    p1.from_h( p2.to_h )
+      @p.set_resource( :Foo, 'create', described_class::ALLOW )
+      @p.set_resource( :Foo, 'update', described_class::ASK   )
+      @p.set_resource( :Foo, 'delete', described_class::DENY  )
 
-    expect( p1.to_h ).to eq({
-      'default' => {
-        'actions' => {
-          'create' => described_class::ALLOW,
-          'update' => described_class::ASK,
-          'delete' => described_class::DENY
+      @p.set_resource_fallback( :Foo, described_class::ALLOW )
+
+      @expected = {
+        'resources' => {
+          'Foo' => {
+            'actions' => {
+              'create' => described_class::ALLOW,
+              'update' => described_class::ASK,
+              'delete' => described_class::DENY
+            },
+            'else' => described_class::ALLOW
+          },
         },
-        'else' => described_class::ALLOW
+        'default' => {
+          'actions' => {
+            'create' => described_class::ASK,
+            'update' => described_class::DENY,
+            'delete' => described_class::ALLOW
+          },
+          'else' => described_class::DENY
+        }
       }
-    })
+    end
+
+    it 'can be loaded into another instance as a Hash' do
+      p = described_class.new
+      p.from_h( @p.to_h )
+      expect( p.to_h ).to eq( @expected )
+    end
+
+    it 'can be used to initialise a new instance as a Hash' do
+      p = described_class.new( @p.to_h )
+      expect( p.to_h ).to eq( @expected )
+    end
+
+    it 'allows correctly' do
+      expect( @p.permitted?( :Foo, :create ) ).to eq( described_class::ALLOW )
+      expect( @p.permitted?( :Foo, :show ) ).to eq( described_class::ALLOW )
+      expect( @p.permitted?( :Foo, :list ) ).to eq( described_class::ALLOW )
+    end
+
+    it 'asks correctly' do
+      expect( @p.permitted?( :Foo, :update ) ).to eq( described_class::ASK )
+    end
+
+    it 'denies correctly' do
+      expect( @p.permitted?( :Foo, :delete ) ).to eq( described_class::DENY )
+    end
+
+    it 'action defaults work' do
+      expect( @p.permitted?( :Bar, :create ) ).to eq( described_class::ASK )
+      expect( @p.permitted?( :Bar, :update ) ).to eq( described_class::DENY )
+      expect( @p.permitted?( :Bar, :delete ) ).to eq( described_class::ALLOW )
+    end
+
+    it 'fallback default works' do
+      expect( @p.permitted?( :Bar, :show ) ).to eq( described_class::DENY )
+      expect( @p.permitted?( :Bar, :list ) ).to eq( described_class::DENY )
+    end
   end
 end
