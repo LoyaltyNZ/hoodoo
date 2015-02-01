14044499aa840b2056160ed05a75cbf63e42d8c5
Joseph Leniston <joseph.leniston@abletech.co.nz>
2015-02-02 08:36:31 +1300

    Add Refund resource

---
+++ lib/hoodoo/data/resources/refund.rb
@@ -0,0 +1,34 @@
+########################################################################
+# File::    refund.rb
+# (C)::     Loyalty New Zealand 2015
+#
+# Purpose:: Define documented Platform API Resource 'Refund'.
+# ----------------------------------------------------------------------
+#           27-Jan-2015 (JML): Created.
+########################################################################
+
+module Hoodoo
+  module Data
+    module Resources
+
+      # Documented Platform API Resource 'Refund'.
+      #
+      class Refund < Hoodoo::Presenters::Base
+
+        schema do
+          text   :token_identifier
+
+          object :basket, :required => true do
+            type :Basket
+          end
+
+          text   :pos_reference
+          uuid   :purchase_id, :resource => :Purchase
+
+          array  :calculations
+        end
+
+      end
+    end
+  end
+end
