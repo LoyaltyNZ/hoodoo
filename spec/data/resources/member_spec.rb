1a2976223448eaa096ef391c54534b41531b5bb9
thelollies <rorystephenson@gmail.com>
2015-02-23 11:22:09 +1300

    Remove Member demographics

--- spec/data/resources/member_spec.rb
+++ spec/data/resources/member_spec.rb
@@ -4,21 +4,12 @@ describe Hoodoo::Data::Resources::Member do
   it 'should match schema expectations' do
     schema = described_class.get_schema()
 
-    expect(schema.is_internationalised?()).to eq(true)
+    expect(schema.is_internationalised?()).to eq(false)
 
-    expect(schema.properties.count).to eq(4)
+    expect(schema.properties.count).to eq(1)
 
     expect(schema.properties['account_id']).to be_a(Hoodoo::Presenters::UUID)
     expect(schema.properties['account_id'].resource).to eq(:Account)
     expect(schema.properties['account_id'].required).to eq(false)
-
-    expect(schema.properties['formal_name']).to be_a(Hoodoo::Presenters::Text)
-    expect(schema.properties['formal_name'].required).to eq(true)
-
-    expect(schema.properties['informal_name']).to be_a(Hoodoo::Presenters::Text)
-    expect(schema.properties['informal_name'].required).to eq(false)
-
-    expect(schema.properties['date_of_birth']).to be_a(Hoodoo::Presenters::Date)
-    expect(schema.properties['date_of_birth'].required).to eq(true)
   end
 end
