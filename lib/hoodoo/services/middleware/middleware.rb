dd4e2d17c6dd1635307b9ebb39056dd2b94c9666
Graham Jenson <grahamjenson@maori.geek.nz>
2015-02-26 15:00:17 +1300

    fixing more tests?

--- lib/hoodoo/services/middleware/middleware.rb
+++ lib/hoodoo/services/middleware/middleware.rb
@@ -2213,11 +2213,12 @@ module Hoodoo; module Services
 
         # This isn't an array, it's an AugmentedHash describing errors. Turn
         # this into a formal errors collection.
-        if on_queue
+        if response.respond_to?(:status_code)
           code = response.status_code
         else
           code = response.code
         end
+        
         errors_from_other_resource = Hoodoo::Errors.new()
         parsed[ 'errors' ].each do | error |
           errors_from_other_resource.add_precompiled_error(
