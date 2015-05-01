10a64b81a8c658be63ab45fbe83c898c17d06944
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2015-05-01 12:38:03 +1200

    Better test coverage for secure_with extensions

--- spec/active/active_record/secure_spec.rb
+++ spec/active/active_record/secure_spec.rb
@@ -4,7 +4,8 @@ require 'active_record'
 describe Hoodoo::ActiveRecord::Secure do
   before :all do
     spec_helper_silence_stdout() do
-      ActiveRecord::Migration.create_table( :r_spec_model_secure_tests ) do | t |
+
+      migration = Proc.new do | t |
         t.text :creator
         t.text :distributor
         t.text :field
@@ -12,35 +13,47 @@ describe Hoodoo::ActiveRecord::Secure do
         t.timestamps
       end
 
-      class RSpecModelSecureTest < ActiveRecord::Base
+      ActiveRecord::Migration.create_table( :r_spec_model_secure_test_as, &migration )
+      ActiveRecord::Migration.create_table( :r_spec_model_secure_test_bs, &migration )
+
+      class RSpecModelSecureTestA < ActiveRecord::Base
         include Hoodoo::ActiveRecord::Secure
 
         secure_with(
-          :creator      => 'authorised_creators',   # Array
-          'distributor' => 'authorised_distributor' # Single item
+          :creator      => 'authorised_creators',  # Array
+          'distributor' => :authorised_distributor # Single item
+        )
+      end
+
+      class RSpecModelSecureTestB < ActiveRecord::Base
+        include Hoodoo::ActiveRecord::Secure
+
+        secure_with(
+          :creator      => { :session_field_name => 'authorised_creators'   },
+          'distributor' => { :session_field_name => :authorised_distributor } # Single item
         )
       end
     end
   end
 
-  context '#secure and #secure_with' do
+  shared_examples 'a secure model' do
     before :each do
-      @scoped_1 = RSpecModelSecureTest.new
+      @scoped_1 = @class_to_test.new
       @scoped_1.creator     =     'creator 1'
       @scoped_1.distributor = 'distributor 1'
-      @scoped_1.field            = 'scoped 1'
+      @scoped_1.field       =      'scoped 1'
       @scoped_1.save!
 
-      @scoped_2 = RSpecModelSecureTest.new
-      @scoped_2.creator         = 'creator 1'
+      @scoped_2 = @class_to_test.new
+      @scoped_2.creator     =     'creator 1'
       @scoped_2.distributor = 'distributor 2'
-      @scoped_2.field            = 'scoped 2'
+      @scoped_2.field       =      'scoped 2'
       @scoped_2.save!
 
-      @scoped_3 = RSpecModelSecureTest.new
-      @scoped_3.creator         = 'creator 2'
+      @scoped_3 = @class_to_test.new
+      @scoped_3.creator     =     'creator 2'
       @scoped_3.distributor = 'distributor 2'
-      @scoped_3.field            = 'scoped 3'
+      @scoped_3.field       =      'scoped 3'
       @scoped_3.save!
 
       # Get a good-enough-for-test interaction which has a context
@@ -61,90 +74,100 @@ describe Hoodoo::ActiveRecord::Secure do
     it 'finds with secure scopes from the class' do
       @session.scoping = { :authorised_creators => [ 'creator 1' ], :authorised_distributor => 'distributor 1' }
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to eq( @scoped_1 )
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_2.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_2.id )
       expect( found ).to be_nil
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_3.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_3.id )
       expect( found ).to be_nil
 
       @session.scoping.authorised_distributor = 'distributor 2'
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to be_nil
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_2.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_2.id )
       expect( found ).to eq( @scoped_2 )
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_3.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_3.id )
       expect( found ).to be_nil
 
       @session.scoping.authorised_creators = [ 'creator 2' ]
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to be_nil
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_2.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_2.id )
       expect( found ).to be_nil
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_3.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_3.id )
       expect( found ).to eq( @scoped_3 )
 
       @session.scoping.authorised_creators = [ 'creator 1', 'creator 2' ]
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to be_nil
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_2.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_2.id )
       expect( found ).to eq( @scoped_2 )
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_3.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_3.id )
       expect( found ).to eq( @scoped_3 )
     end
 
     it 'finds with secure scopes with a chain' do
       @session.scoping = { :authorised_creators => [ 'creator 1' ], :authorised_distributor => 'distributor 1' }
 
-      found = RSpecModelSecureTest.where( :field => @scoped_1.field ).secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.where( :field => @scoped_1.field ).secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to eq( @scoped_1 )
 
-      found = RSpecModelSecureTest.where( :field => @scoped_1.field + '!' ).secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.where( :field => @scoped_1.field + '!' ).secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to be_nil
 
-      found = RSpecModelSecureTest.where( :field => @scoped_2.field ).secure( @context ).find_by_id( @scoped_2.id )
+      found = @class_to_test.where( :field => @scoped_2.field ).secure( @context ).find_by_id( @scoped_2.id )
       expect( found ).to be_nil
 
-      found = RSpecModelSecureTest.where( :field => @scoped_3.field ).secure( @context ).find_by_id( @scoped_3.id )
+      found = @class_to_test.where( :field => @scoped_3.field ).secure( @context ).find_by_id( @scoped_3.id )
       expect( found ).to be_nil
 
       @session.scoping.authorised_creators = [ 'creator 1', 'creator 2' ]
       @session.scoping.authorised_distributor  = 'distributor 2'
 
-      found = RSpecModelSecureTest.where( :field => @scoped_1.field ).secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.where( :field => @scoped_1.field ).secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to be_nil
 
-      found = RSpecModelSecureTest.where( :field => @scoped_2.field ).secure( @context ).find_by_id( @scoped_2.id )
+      found = @class_to_test.where( :field => @scoped_2.field ).secure( @context ).find_by_id( @scoped_2.id )
       expect( found ).to eq( @scoped_2 )
 
-      found = RSpecModelSecureTest.where( :field => @scoped_3.field ).secure( @context ).find_by_id( @scoped_3.id )
+      found = @class_to_test.where( :field => @scoped_3.field ).secure( @context ).find_by_id( @scoped_3.id )
       expect( found ).to eq( @scoped_3 )
 
-      found = RSpecModelSecureTest.where( :field => @scoped_3.field + '!' ).secure( @context ).find_by_id( @scoped_3.id )
+      found = @class_to_test.where( :field => @scoped_3.field + '!' ).secure( @context ).find_by_id( @scoped_3.id )
       expect( found ).to be_nil
     end
 
     it 'finds nothing if scope lacks required keys' do
       @session.scoping = { :authorised_distributor => 'distributor 1' }
 
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to be_nil
     end
 
     it 'finds nothing if scope is missing' do
-      found = RSpecModelSecureTest.secure( @context ).find_by_id( @scoped_1.id )
+      found = @class_to_test.secure( @context ).find_by_id( @scoped_1.id )
       expect( found ).to be_nil
     end
+  end
+
+  context 'works with a Symbol' do
+    before( :all ) { @class_to_test = RSpecModelSecureTestA }
+    it_behaves_like 'a secure model'
+  end
+
+  context 'works with a Hash' do
+    before( :all ) { @class_to_test = RSpecModelSecureTestB }
+    it_behaves_like 'a secure model'
   end
 end
