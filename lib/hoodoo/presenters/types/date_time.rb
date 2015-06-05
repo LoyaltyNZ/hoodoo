8bfccb71b597a48f26ba976d2e6a638345480d45
Graham Jenson <grahamjenson@maori.geek.nz>
2015-06-05 12:22:15 +0100

    losening constraints to match more iso8601 dates

--- lib/hoodoo/presenters/types/date_time.rb
+++ lib/hoodoo/presenters/types/date_time.rb
@@ -8,8 +8,10 @@ module Hoodoo
         errors = super data, path
         return errors if errors.has_errors? || (!@required and data.nil?)
 
+        #borrowed from http://www.pelagodesign.com/blog/2009/05/20/iso-8601-date-validation-that-doesnt-suck/
+        regex = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
         begin
-          valid = (/(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})(Z|[+-](\d{2})\:(\d{2}))/=~data.to_s) == 0 && data.size>10 && ::DateTime.parse(data).is_a?(::DateTime)
+          valid = (regex=~data.to_s) == 0 && ::DateTime.parse(data).is_a?(::DateTime)
         rescue ArgumentError
           valid = false
         end
