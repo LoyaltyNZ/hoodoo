3a25953de1ded33eed8a34d92dd2d7b717cd902d
thelollies <rorystephenson@gmail.com>
2015-06-29 16:27:04 +1200

    Configurable UUID column.

--- lib/hoodoo/active/active_record/uuid.rb
+++ lib/hoodoo/active/active_record/uuid.rb
@@ -68,13 +68,92 @@ module Hoodoo
       #           this module.
       #
       def self.instantiate( model )
+        model.extend( ClassMethods )
+
         model.primary_key = 'id'
 
         model.before_validation do
-          self.id = Hoodoo::UUID.generate() if self.id.nil?
+          if self.send( self.class.uuid_column ).nil?
+            self.send( "#{self.class.uuid_column}=", Hoodoo::UUID.generate() )
+          end
         end
 
-        model.validates( :id, uuid: true, presence: true, uniqueness: true )
+        model.validate do | instance |
+
+          # Validator options
+          v_opts = {
+            :attributes => [ self.class.uuid_column ],
+            :class => self.class
+          }
+
+          # Note that the unique UUID validator is conditionally added second
+          # rather than last to maintain the legacy order of these validations
+          # which some tests may rely on.
+          validator_instances = [
+            ::UuidValidator.new( v_opts.dup ),
+            ::ActiveRecord::Validations::UniquenessValidator.new( v_opts.dup )
+          ]
+          if instance.class.validate_uuid_uniqueness?
+            validator_instances.insert( 1, ::ActiveRecord::Validations::PresenceValidator.new( v_opts.dup ) )
+          end
+
+          validator_instances.each do | validator_instance |
+            validator_instance.validate_each(
+              self,
+              self.class.uuid_column,
+              self.send( self.class.uuid_column )
+            )
+          end
+
+        end
+
+      end
+
+      module ClassMethods
+
+        # The name of the column which stores the UUID. This can be set via
+        # #uuid_column=, defaulting to :id.
+        #
+        def uuid_column
+          if class_variable_defined?( :@@hoodoo_uuid_column )
+            return class_variable_get( :@@hoodoo_uuid_column )
+          else
+            return :id
+          end
+        end
+
+        # Set the name of the column which stores the uuid. This can be read via
+        # #uuid_column, defaulting to :id.
+        #
+        # +uuid_column_name+:: The symbolised name of the column which holds the
+        #                      UUID.
+        #
+        def uuid_column=( uuid_column_name )
+          class_variable_set( :@@hoodoo_uuid_column, uuid_column_name )
+        end
+
+        # True if the UUID of the model should be validated to be unqiue. This
+        # can be set via #validate_uuid_uniqueness=, defaulting to true.
+        #
+        def validate_uuid_uniqueness?
+          if class_variable_defined?( :@@hoodoo_unique_uuid_column )
+            return class_variable_get( :@@hoodoo_unique_uuid_column )
+          else
+            return true
+          end
+        end
+
+        # Set whether the UUID of the model should be validated to be unique.
+        # This can be checked via #validate_uuid_uniqueness? and defaults to
+        # true.
+        #
+        # +is_unique+:: Boolean value, true if uuid column should be validated
+        #               as unique.
+        #
+        def validate_uuid_uniqueness=( validate_uuid_uniqueness )
+          class_variable_set( :@@hoodoo_unique_uuid_column, validate_uuid_uniqueness )
+        end
+
       end
 
     end
