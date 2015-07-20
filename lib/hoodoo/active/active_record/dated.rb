3eddbf0267bba3a920662c2c4a479de6c0593a53
thelollies <rorystephenson@gmail.com>
2015-07-21 11:40:41 +1200

    WIP specs randomly failing but functionality is working.

--- lib/hoodoo/active/active_record/dated.rb
+++ lib/hoodoo/active/active_record/dated.rb
@@ -3,16 +3,17 @@
 # (C)::     Loyalty New Zealand 2015
 #
 # Purpose:: Support mixin for models subclassed from ActiveRecord::Base
-#           providing as-per-API-standard internationalisation support.
+#           providing as-per-API-standard dating support.
 # ----------------------------------------------------------------------
 #           14-Jul-2015 (ADH): Created.
+#           21-Jul-2015 (RJS): Functionality implemented.
 ########################################################################
 
 module Hoodoo
   module ActiveRecord
 
     # Support mixin for models subclassed from ActiveRecord::Base providing
-    # as-per-API-standard internationalisation support. See
+    # as-per-API-standard dating support. See
     # Hoodoo::ActiveRecord::Dated::ClassMethods#dated for details.
     #
     # See also:
@@ -41,11 +42,24 @@ module Hoodoo
       # Hoodoo::ActiveRecord::Dated::ClassMethods methods are defined as
       # class methods on the including class.
       #
+      # Additionally a history model is created for interacting with the
+      # history records.
+      #
       # +model+:: The ActiveRecord::Base descendant that is including
       #           this module.
       #
       def self.instantiate( model )
         model.extend( ClassMethods )
+
+        # Define a model for the history entries which is namespaced with the
+        # original model's name. So if the original model is called
+        # Post, the history model will be PostHistoryEntry.
+        history_klass = Class.new( ::ActiveRecord::Base ) do
+          self.primary_key = :id
+          self.table_name  = model.table_name + "_history_entries"
+        end
+        Object.const_set model.history_model_name, history_klass
+
       end
 
       # Collection of class methods that get defined on an including class via
@@ -53,7 +67,9 @@ module Hoodoo
       #
       module ClassMethods
 
-        # TODO: Placeholder
+        # Return an ActiveRecord::Relation containing the model instances which
+        # are effective at context.request.dated_at. If this value is nil the
+        # current time in UTC is used.
         #
         # +context+:: Hoodoo::Services::Context instance describing a call
         #             context. This is typically a value passed to one of
@@ -61,24 +77,135 @@ module Hoodoo
         #             that a resource subclass implements.
         #
         def dated( context )
-          prevailing_scope = all() # "Model.all" -> returns anonymous scope
-          return prevailing_scope
+
+          date_time = context.request.dated_at || Time.now.utc
+
+          return self.dated_at( date_time )
+
         end
 
-        # TODO: Placeholder
+        # Return an ActiveRecord::Relation containing the models instances which
+        # are effective at the specified date_time.
         #
-        # +date_time+:: The Date/Time (as a Ruby Date, Time or DateTime
-        #               instance) for which the "effective dated" scope is to
-        #               be constructed.
+        # +date_time+:: (Optional) The Date/Time (as a Ruby Date, Time or
+        #               DateTime instance) for which the "effective dated" scope
+        #               is to be constructed.
         #
-        def dated_at( date_time )
-          prevailing_scope = all() # "Model.all" -> returns anonymous scope
-          return prevailing_scope
+        def dated_at( date_time = Time.now )
+
+          # Convert the date time to UTC
+          date_time = date_time.utc
+
+          # Create a string that specifies this model's attributes joined by
+          # commas for use in a SQL query.
+          formatted_model_attributes = self.attribute_names.join( ", " )
+
+          # Escape user provided data
+          safe_date_time = sanitize( date_time )
+
+          # A query that combines historical and current records which are
+          # effective at the specified date time.
+          nested_query = %{
+            (
+              SELECT #{ formatted_model_attributes } FROM (
+                SELECT #{ formatted_model_attributes }, updated_at as effective_start, null AS effective_end
+                FROM #{ self.table_name }
+
+                UNION ALL
+
+                SELECT #{ self.history_column_mapping }, effective_start, effective_end
+                FROM #{ self.effective_history_table }
+              ) AS u
+              WHERE effective_start <= #{ safe_date_time } AND (effective_end > #{ safe_date_time } OR effective_end IS NULL)
+            ) AS #{ self.table_name }
+          }
+
+          # Form a query which uses ActiveRecord to list effective records.
+          from( nested_query )
+
         end
 
-        # def dated_with( map )
-        #   class_variable_set( '@@nz_co_loyalty_hoodoo_dated_with', map )
-        # end
+        # Return an ActiveRecord::Relation containing all historical and current
+        # model instances.
+        #
+        def historical_and_current
+
+          # Create a string that specifies this model's attributes joined by
+          # commas for use in a SQL query.
+          formatted_model_attributes = self.attribute_names.join( ", " )
+
+          # A query that combines historical and current records.
+          nested_query = %{
+            (
+              SELECT #{ formatted_model_attributes }
+              FROM #{ self.table_name }
+
+              UNION ALL
+
+              SELECT #{ self.history_column_mapping }
+              FROM #{ self.effective_history_table }
+            ) AS #{ self.table_name }
+          }
+
+          # Form a query which uses ActiveRecord to list effective records.
+          from( nested_query )
+
+        end
+
+        # The String name of the model which represents history entries for this
+        # model.
+        def history_model_name
+
+          self.to_s << "HistoryEntry"
+
+        end
+
+        # Get the symbolised name of the history table for model. This defaults
+        # to the name of the model's table concatenated with _history_entries.
+        # So if the table name is :posts, the history table would be
+        # :posts_history_entries.
+        #
+        def effective_history_table
+
+          self.history_model_name.constantize.table_name
+
+        end
+
+        # The name of this model's primary key. This can be set via the
+        # ActiveRecord setter self.primary_key=(). The default is "id".
+        def model_pkey
+
+          self.primary_key || "id"
+
+        end
+
+        # Set the name of the table which stores the history entries for this
+        # model.
+        #
+        # +effective_start_history_table+:: String or Symbol name of the table.
+        #
+        def effective_date_history_table=( effective_start_history_table )
+
+          history_model_name.constantize.table_name = effective_start_history_table
+
+        end
+
+        protected
+
+        # Forms and returns string which maps history table column names to the
+        # primary table column names for use in SQL queries.
+        #
+        def history_column_mapping
+
+          desired_attributes = self.attribute_names.dup
+
+          primary_key_index = desired_attributes.index( self.model_pkey )
+          desired_attributes[ primary_key_index ] = "uuid as " << desired_attributes[ primary_key_index ]
+
+          desired_attributes.join( ", " )
+
+        end
+
       end
     end
   end
--- spec/active/active_record/dated_spec.rb
+++ spec/active/active_record/dated_spec.rb
@@ -2,19 +2,354 @@ require 'spec_helper'
 require 'active_record'
 
 describe Hoodoo::ActiveRecord::Dated do
-  pending 'is tested' do
 
-    # For RCov only
+  before :all do
+    spec_helper_silence_stdout() do
 
-    class Test
-      include Hoodoo::ActiveRecord::Dated
-      def self.all; end
+      ActiveRecord::Migration.create_table( :r_spec_model_effective_date_tests, :id => false ) do | t |
+        t.text :id,  :null => false
+        t.text :data
+        t.timestamps
+      end
+
+      ActiveRecord::Migration.create_table( :r_spec_model_effective_date_tests_history_entries, :id => false ) do | t |
+        t.text     :id,              :null => false
+        t.text     :uuid,            :null => false
+        t.text     :data
+        t.datetime :effective_start, :null => false
+        t.datetime :effective_end,   :null => false
+        t.timestamps
+      end
+
+      class RSpecModelEffectiveDateTest < ActiveRecord::Base
+        include Hoodoo::ActiveRecord::Dated
+        self.primary_key = :id
+      end
+
+      ActiveRecord::Migration.create_table( :r_spec_model_effective_date_test_overrides, :id => false ) do | t |
+        t.text :activerecord_id,  :null => false
+        t.text :data
+        t.timestamps
+      end
+
+      ActiveRecord::Migration.create_table( :r_spec_model_effective_date_history_entries, :id => false ) do | t |
+        t.text     :id,              :null => false
+        t.text     :uuid,            :null => false
+        t.text     :data
+        t.datetime :effective_start, :null => false
+        t.datetime :effective_end,   :null => false
+        t.timestamps
+      end
+
+      class RSpecModelEffectiveDateTestOverride < ActiveRecord::Base
+        include Hoodoo::ActiveRecord::Dated
+        self.primary_key                  = :activerecord_id
+        self.effective_date_history_table = :r_spec_model_effective_date_history_entries
+      end
+    end
+  end
+
+  context "using default effective dating config" do
+
+    before( :all ) do
+
+      # Create some examples data for finding. The data has two different UUIDs
+      # which I'll referer to as A and B. The following tables contain the
+      # historical and current records separately with their attributes.
+      #
+      # Historical:
+      # -------------------------------------------------------------------
+      #  uuid | data    | created_at    | effective_end | effective_start |
+      # -------------------------------------------------------------------
+      #  A    | "one"   | now - 5 hours | now - 3 hours | now - 5 hours   |
+      #  B    | "two"   | now - 4 hours | now - 2 hours | now - 4 hours   |
+      #  A    | "three" | now - 5 hours | now - 1 hour  | now - 3 hours   |
+      #  B    | "four"  | now - 4 hours | now           | now - 2 hour    |
+      #
+      # Current:
+      # --------------------------------
+      #  uuid | data   | created_at    |
+      # --------------------------------
+      #  B    | "five" | now - 4 hours |
+      #  A    | "six"  | now - 5 hours |
+      #
+
+      @uuid_a = Hoodoo::UUID.generate
+      @uuid_b = Hoodoo::UUID.generate
+
+      @now  = Time.now.utc
+
+      # uuid, data, created_at, effective_end, effective_start
+      [
+        [ @uuid_a, "one",   @now - 5.hours, @now - 3.hours , @now - 5.hours ],
+        [ @uuid_b, "two",   @now - 4.hours, @now - 2.hours , @now - 4.hours ],
+        [ @uuid_a, "three", @now - 5.hours, @now - 1.hour  , @now - 3.hours ],
+        [ @uuid_b, "four",  @now - 4.hours, @now           , @now - 2.hours ]
+      ].each do | row_data |
+        RSpecModelEffectiveDateTestHistoryEntry.new( {
+          :id              => row_data[ 0 ] + "-" + row_data[ 3 ].iso8601,
+          :uuid            => row_data[ 0 ],
+          :data            => row_data[ 1 ],
+          :created_at      => row_data[ 2 ],
+          :effective_end   => row_data[ 3 ],
+          :effective_start => row_data[ 4 ]
+        } ).save!
+      end
+
+      # uuid, data, created_at, updated_at
+      [
+        [ @uuid_b, "five", @now - 4.hours, @now ],
+        [ @uuid_a, "six",  @now - 5.hours, @now - 1.hour ]
+      ].each do | row_data |
+        RSpecModelEffectiveDateTest.new( {
+          :id         => row_data[ 0 ],
+          :data       => row_data[ 1 ],
+          :created_at => row_data[ 2 ],
+          :updated_at => row_data[ 3 ]
+        } ).save!
+      end
+
     end
 
-    ignored = true
-    Test.dated( ignored )
-    Test.dated_at( ignored )
+    context '.dated_at' do
+      it 'returns counts correctly' do
+        expect( RSpecModelEffectiveDateTest.dated_at( @now - 10.hours ).count ).to be 0
+        expect( RSpecModelEffectiveDateTest.dated_at( @now ).count ).to_not be 0
+      end
 
-    raise "Replace with real test"
+      def test_expectation( time, expected_data )
+        expect( RSpecModelEffectiveDateTest.dated_at( time ).pluck( :data ) ).to match_array( expected_data )
+      end
+
+      it 'returns no records before any were effective' do
+        test_expectation( @now - 10.hours, [] )
+      end
+
+      it 'returns records that used to be effective starting at past time' do
+        test_expectation( @now - 5.hours, [ "one" ] )
+        test_expectation( @now - 4.hours, [ "one", "two" ] )
+        test_expectation( @now - 3.hours, [ "two", "three" ] )
+        test_expectation( @now - 2.hours, [ "three", "four" ] )
+        test_expectation( @now - 1.hour,  [ "four", "six" ] )
+      end
+
+      it 'returns records that are effective now' do
+        test_expectation( @now, [ "five", "six" ] )
+      end
+
+      it 'works with further filtering' do
+        expect( RSpecModelEffectiveDateTest.dated_at( @now ).where( :id => @uuid_a ).pluck( :data ) ).to eq( [ "six" ] )
+      end
+
+    end
+
+    context '.dated' do
+      it 'returns counts correctly' do
+        # The contents of the Context are irrelevant aside from the fact that it
+        # needs a request to store the dated_at value.
+        request = Hoodoo::Services::Request.new
+        context = Hoodoo::Services::Context.new( nil, request, nil, nil )
+
+        context.request.dated_at = @now - 10.hours
+        expect( RSpecModelEffectiveDateTest.dated( context ).count ).to be 0
+
+        context.request.dated_at = @now
+        expect( RSpecModelEffectiveDateTest.dated( context ).count ).to_not be 0
+      end
+
+      def test_expectation( time, expected_data )
+        # The contents of the Context are irrelevant aside from the fact that it
+        # needs a request to store the dated_at value.
+        request = Hoodoo::Services::Request.new
+        context = Hoodoo::Services::Context.new( nil, request, nil, nil )
+        context.request.dated_at = time
+
+        expect( RSpecModelEffectiveDateTest.dated( context ).pluck( :data ) ).to match_array( expected_data )
+      end
+
+      it 'returns no records before any were effective' do
+        test_expectation( @now - 10.hours, [] )
+      end
+
+      it 'returns records that used to be effective starting at past time' do
+        test_expectation( @now - 5.hours, [ "one" ] )
+        test_expectation( @now - 4.hours, [ "one", "two" ] )
+        test_expectation( @now - 3.hours, [ "two", "three" ] )
+        test_expectation( @now - 2.hours, [ "three", "four" ] )
+        test_expectation( @now - 1.hour,  [ "four", "six" ] )
+      end
+
+      it 'returns records that are effective now' do
+        test_expectation( @now, [ "five", "six" ] )
+      end
+
+      it 'works with further filtering' do
+
+        # The contents of the Context are irrelevant aside from the fact that it
+        # needs a request to store the dated_at value.
+        request = Hoodoo::Services::Request.new
+        context = Hoodoo::Services::Context.new( nil, request, nil, nil )
+        context.request.dated_at = @now
+
+        expect( RSpecModelEffectiveDateTest.dated( context ).where( :id => @uuid_a ).pluck( :data ) ).to eq( [ "six" ] )
+      end
+
+    end
+
+    context '.historical_and_current' do
+
+      it 'returns counts correctly' do
+        expect( RSpecModelEffectiveDateTest.historical_and_current.count ).to_not be 0
+      end
+
+      it 'lists all historical and current records' do
+        expect( RSpecModelEffectiveDateTest.historical_and_current.pluck( :data ) ).to match_array( [ 'one', 'two', 'three', 'four', 'five', 'six' ] )
+      end
+
+    end
   end
+
+  context "overriding primary key name and history table name" do
+
+    before( :all ) do
+
+      # Create some examples data for finding. The data has two different UUIDs
+      # which I'll referer to as A and B. The following tables contain the
+      # historical and current records separately with their attributes.
+      #
+      # Historical:
+      # -------------------------------------------------------------------
+      #  uuid | data    | created_at    | effective_end | effective_start |
+      # -------------------------------------------------------------------
+      #  A    | "one"   | now - 5 hours | now - 3 hours | now - 5 hours   |
+      #  B    | "two"   | now - 4 hours | now - 2 hours | now - 4 hours   |
+      #  A    | "three" | now - 5 hours | now - 1 hour  | now - 3 hours   |
+      #  B    | "four"  | now - 4 hours | now           | now - 2 hours   |
+      #
+      # Current:
+      # -------------------------------------------
+      #  activerecord_id | data   | created_at    |
+      # -------------------------------------------
+      #  B               | "five" | now - 4 hours |
+      #  A               | "six"  | now - 5 hours |
+      #
+
+      @uuid_a = Hoodoo::UUID.generate
+      @uuid_b = Hoodoo::UUID.generate
+
+      @now  = Time.now.utc
+
+      # uuid, data, created_at, effective_at
+      [
+        [ @uuid_a, "one",   @now - 5.hours, @now - 3.hours, @now - 5.hours ],
+        [ @uuid_b, "two",   @now - 4.hours, @now - 2.hours, @now - 4.hours ],
+        [ @uuid_a, "three", @now - 5.hours, @now - 1.hour,  @now - 3.hours ],
+        [ @uuid_b, "four",  @now - 4.hours, @now,           @now - 2.hours ]
+      ].each do | row_data |
+        RSpecModelEffectiveDateTestOverrideHistoryEntry.new( {
+          :id                => row_data[ 0 ] + "-" + row_data[ 3 ].iso8601,
+          :uuid            => row_data[ 0 ],
+          :data            => row_data[ 1 ],
+          :created_at      => row_data[ 2 ],
+          :effective_end   => row_data[ 3 ],
+          :effective_start => row_data[ 4 ]
+        } ).save!
+      end
+
+      # uuid, data, created_at
+      [
+        [ @uuid_b, "five", @now - 4.hours, @now ],
+        [ @uuid_a, "six",  @now - 5.hours, @now - 1.hour ]
+      ].each do | row_data |
+        RSpecModelEffectiveDateTestOverride.new( {
+          :id         => row_data[ 0 ],
+          :data       => row_data[ 1 ],
+          :created_at => row_data[ 2 ],
+          :updated_at => row_data[ 3 ]
+        } ).save!
+      end
+
+    end
+
+    context '.dated_at' do
+
+      def test_expectation( time, expected_data )
+        expect( RSpecModelEffectiveDateTestOverride.dated_at( time ).pluck( :data ) ).to match_array( expected_data )
+      end
+
+      it 'returns no records before any were effective' do
+        test_expectation( @now - 10.hours, [] )
+      end
+
+      it 'returns records that used to be effective starting at past time' do
+        test_expectation( @now - 5.hours, [ "one" ] )
+        test_expectation( @now - 4.hours, [ "one", "two" ] )
+        test_expectation( @now - 3.hours, [ "two", "three" ] )
+        test_expectation( @now - 2.hours, [ "three", "four" ] )
+        test_expectation( @now - 1.hour,  [ "four", "six" ] )
+      end
+
+      it 'returns records that are effective now' do
+        test_expectation( @now, [ "five", "six" ] )
+      end
+
+      it 'works with further filtering' do
+        found = RSpecModelEffectiveDateTestOverride.dated_at( @now ).where( :activerecord_id => @uuid_a )
+        expect( found.pluck( :data ) ).to eq( [ "six" ] )
+      end
+
+    end
+
+    context '.dated' do
+
+      def test_expectation( time, expected_data )
+        # The contents of the Context are irrelevant aside from the fact that it
+        # needs a request to store the dated_at value.
+        request = Hoodoo::Services::Request.new
+        context = Hoodoo::Services::Context.new( nil, request, nil, nil )
+        context.request.dated_at = time
+
+        expect( RSpecModelEffectiveDateTestOverride.dated( context ).pluck( :data ) ).to match_array( expected_data )
+      end
+
+      it 'returns no records before any were effective' do
+        test_expectation( @now - 10.hours, [] )
+      end
+
+      it 'returns records that used to be effective starting at past time' do
+        test_expectation( @now - 5.hours, [ "one" ] )
+        test_expectation( @now - 4.hours, [ "one", "two" ] )
+        test_expectation( @now - 3.hours, [ "two", "three" ] )
+        test_expectation( @now - 2.hours, [ "three", "four" ] )
+        test_expectation( @now - 1.hour,  [ "four", "six" ] )
+      end
+
+      it 'returns records that are effective now' do
+        test_expectation( @now, [ "five", "six" ] )
+      end
+
+      it 'works with further filtering' do
+
+        # The contents of the Context are irrelevant aside from the fact that it
+        # needs a request to store the dated_at value.
+        request = Hoodoo::Services::Request.new
+        context = Hoodoo::Services::Context.new( nil, request, nil, nil )
+        context.request.dated_at = @now
+
+        found = RSpecModelEffectiveDateTestOverride.dated( context ).where( :activerecord_id => @uuid_a )
+        expect( found.pluck( :data ) ).to eq( [ "six" ] )
+      end
+
+    end
+
+
+    context '.historical_and_current' do
+
+      it 'lists all historical and current records' do
+        expect( RSpecModelEffectiveDateTestOverride.historical_and_current.pluck( :data ) ).to match_array( [ 'one', 'two', 'three', 'four', 'five', 'six' ] )
+      end
+
+    end
+  end
+
 end
