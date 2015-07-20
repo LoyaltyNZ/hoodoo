303686a1dc2fc7e777fdee3ee22b68065d16d73c
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2015-07-20 17:19:54 +1200

    Improved test coverage, including mock coverage for to-be-done things so that RCov 100% is meaningful again and accidental coverage omissions will be easier to see

--- spec/active/active_record/dated_spec.rb
+++ spec/active/active_record/dated_spec.rb
@@ -3,6 +3,18 @@ require 'active_record'
 
 describe Hoodoo::ActiveRecord::Dated do
   pending 'is tested' do
-    raise 'TODO'
+
+    # For RCov only
+
+    class Test
+      include Hoodoo::ActiveRecord::Dated
+      def self.all; end
+    end
+
+    ignored = true
+    Test.dated( ignored )
+    Test.dated_at( ignored )
+
+    raise "Replace with real test"
   end
 end
--- spec/active/active_record/translated_spec.rb
+++ spec/active/active_record/translated_spec.rb
@@ -3,6 +3,17 @@ require 'active_record'
 
 describe Hoodoo::ActiveRecord::Translated do
   pending 'is tested' do
-    raise 'TODO'
+
+    # For RCov only
+
+    class Test
+      include Hoodoo::ActiveRecord::Translated
+      def self.all; end
+    end
+
+    ignored = true
+    Test.translated( ignored )
+
+    raise "Replace with real test"
   end
 end
