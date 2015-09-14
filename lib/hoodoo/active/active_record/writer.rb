89edb5e951d3941b1f64f5da223adeb3a174b939
Charles Peach <me@charlespeach.com>
2015-09-15 10:42:20 +1200

    Add Initial persist_in helper implementation and working spec

--- lib/hoodoo/active/active_record/writer.rb
+++ lib/hoodoo/active/active_record/writer.rb
@@ -32,19 +32,15 @@ module Hoodoo
       # Example:
       #
       #     class SomeModel < ActiveRecord::Base
-      #       include Hoodoo::ActiveRecord::Writer
+      #       include Hoodoo::ActiveRecord::Wrier
       #       # ...
       #     end
       #
-      # +model+:: The ActiveRecord::Base descendant that is including
+      # +model+:: The ActiveRecord::Base descendant class that is including
       #           this module.
       #
       def self.included( model )
-        unless model == Hoodoo::ActiveRecord::Base
-          model.send( :include, Hoodoo::ActiveRecord::Writer )
-          instantiate( model )
-        end
-
+        instantiate( model ) unless model == Hoodoo::ActiveRecord::Base
         super( model )
       end
 
@@ -59,22 +55,66 @@ module Hoodoo
         model.extend( ClassMethods )
       end
 
-      def self.persist_in( context, attributes = {}, options = {} )
-        instance = self.new( attributes )
-
-        if block_given?
-          yield( instance )
-        end
-
-        # (maybe with locking, do...)
-
-        instance.save # <-- somewhere in here, we handle the duplicates case
-      end
-
       # Collection of class methods that get defined on an including class via
       # Hoodoo::ActiveRecord::Writer::included.
       #
       module ClassMethods
+
+        # A concurrent safe way to take attributes for a given resource
+        # and save them within a safety net for protection against the inherit
+        # lack of idempotency.
+        #
+        # Regardless of the level of validation (database, or model validations)
+        # this will catch those uniqueness constraint errors and ensure that the
+        # error is added to +context.response+
+        #
+        def persist_in( context, attributes:, instance: )
+          # We either get a ready-initialised instance in the 'instance' option or
+          # have to bulid one from the 'attributes' option, which takes precedence.
+
+          instance = self.new( attributes ) if attributes.present?
+
+          # If this model has an ActiveRecord uniqueness validation, it is still
+          # subject to race conditions and should be backed by a database constraint.
+          # If this constraint fails, try to re-run model validations just in case it
+          # was a race condition case; though of course, it could be that there is
+          # *only* a database constraint and no model validation. If there is *only*
+          # a model validation, the model is ill-defined and at risk.
+
+          # First just see if we have any problems saving anyway.
+          #
+          errors_occurred = begin
+            :any unless instance.save
+          rescue ::ActiveRecord::RecordNotUnique => error
+            :duplication
+          end
+
+          # If there was a problem, just try adding model-originated mapped errors
+          # via the relevant mixin. If there are no errors in context, add one for
+          # the invalid duplication case - no uniqueness validations must be on the
+          # model.
+          #
+          unless errors_occurred.nil?
+            context.response.add_errors( instance.platform_errors() ) if instance.respond_to?( :platform_errors )
+
+            # We do nothing else. Hoodoo will check for errors on exit and if it
+            # sees only duplication-related errors, plus an inbound flag saying
+            # that the caller is allowing these, it'll transform the response into
+            # a 204. If it sees any other errors, normal processing happens.
+            if errors_occurred == :duplication && context.response.halt_processing? == false
+              context.response.add_error(
+                'generic.invalid_duplication',
+                {
+                  :message   => 'Cannot create this resource instance due to a uniqueness constraint violation',
+                  :reference => { :field_name => 'unknown' }
+                }
+              )
+            end
+          end
+
+          return instance
+        end
+
       end
     end
   end
---
+++ spec/active/active_record/writer_spec.rb
@@ -0,0 +1,85 @@
+require 'spec_helper'
+require 'active_record'
+
+describe Hoodoo::ActiveRecord::Writer do
+  before :all do
+
+    ###########################################################################
+    #                         DATABASE AND MODEL SETUP
+    ###########################################################################
+
+    spec_helper_silence_stdout() do
+      ActiveRecord::Migration.create_table(:r_spec_model_writer_tests, :id => false) do | t |
+        t.text :id
+        t.text :uuid
+        t.text :code
+        t.text :random_field
+
+        t.timestamps
+      end
+
+      ActiveRecord::Migration.add_index(:r_spec_model_writer_tests, :code, unique: true)
+    end
+
+    class RSpecModelWriterTestWithValidation < ActiveRecord::Base
+      self.primary_key = :id
+      self.table_name = :r_spec_model_writer_tests
+
+      include Hoodoo::ActiveRecord::Writer
+
+      validates :code, uniqueness: true
+    end
+
+    class RSpecModelWriterTestWithoutValidation < ActiveRecord::Base
+      self.primary_key = :id
+      self.table_name = :r_spec_model_writer_tests
+
+      include Hoodoo::ActiveRecord::Writer
+    end
+  end
+
+  ###########################################################################
+  #                                 TESTS
+  ###########################################################################
+
+  context 'persist_in' do
+
+    before(:each) do
+      @record_with_app_validation = RSpecModelWriterTestWithValidation.create(
+        id: 'one',
+        code: 'unique',
+        random_field: 'sudo random value'
+      )
+
+      @record_without_app_validation = RSpecModelWriterTestWithoutValidation.create(
+        id: 'two',
+        code: 'unique - but only far as the db is concerned',
+        random_field: 'sudo random value'
+      )
+    end
+
+    before(:each) do
+      @interaction = Hoodoo::Services::Middleware::Interaction.new( {}, nil )
+      @interaction.context = Hoodoo::Services::Context.new(
+        Hoodoo::Services::Session.new,
+        @interaction.context.request,
+        @interaction.context.response,
+        @interaction
+      )
+
+      @context = @interaction.context
+      @session = @interaction.context.session
+    end
+
+    it 'adds an invalid duplicate error where AR validations are missing' do
+      record = RSpecModelWriterTestWithoutValidation.new(@record_without_app_validation.attributes)
+      expect(RSpecModelWriterTestWithoutValidation.persist_in(@context, attributes: nil, instance: record)).to be_truthy
+
+      expect(@context.response.errors.errors).to eq(
+        [{"code"=>"generic.invalid_duplication",
+          "message"=>"Cannot create this resource instance due to a uniqueness constraint violation",
+          "reference"=>"unknown"}]
+      )
+    end
+  end
+end
