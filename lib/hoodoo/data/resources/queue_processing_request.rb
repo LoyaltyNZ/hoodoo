e12d85bcdb351d1c7a89a267ce964db8a38c6457
thelollies <rorystephenson@gmail.com>
2015-09-22 10:28:55 +1200

    Add QueueProcessingRequest

---
+++ lib/hoodoo/data/resources/queue_processing_request.rb
@@ -0,0 +1,23 @@
+########################################################################
+# File::    queue_processing_request.rb
+# (C)::     Loyalty New Zealand 2015
+#
+# Purpose:: Define documented Internal Platform API Resource
+#           'QueueProcessingRequest'.
+# ----------------------------------------------------------------------
+#           22-Seo-2015 (RJS): Created.
+########################################################################
+
+module Hoodoo
+  module Data
+    module Resources
+
+      # Documented Platform API Resource 'QueueProcessingRequest'.
+      #
+      class QueueProcessingRequest < Hoodoo::Presenters::Base
+        schema do
+        end
+      end
+    end
+  end
+end
