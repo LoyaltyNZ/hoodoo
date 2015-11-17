2b491e4ad48e07403dc14dbc8646277a3187b5e7
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2015-11-17 16:50:01 +1300

    Add missing file header comments

--- lib/hoodoo/version.rb
+++ lib/hoodoo/version.rb
@@ -1,3 +1,12 @@
+########################################################################
+# File::    version.rb
+# (C)::     Loyalty New Zealand 2015
+#
+# Purpose:: Declare this gem's version number.
+# ----------------------------------------------------------------------
+#           27-Jan-2015 (ADH): Created (for gem under name Hoodoo).
+########################################################################
+
 module Hoodoo
 
   # The Hoodoo gem version.
