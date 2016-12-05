cc1c8a6bb7b6c084e361053cfe891eac714f5694
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2016-12-05 15:13:35 +1300

    NewRelic fakery to bring test coverage *finally* back up to 100%

--- spec/monkey/patch/newrelic_middleware_analytics_spec.rb
+++ spec/monkey/patch/newrelic_middleware_analytics_spec.rb
@@ -3,13 +3,58 @@ require 'spec_helper.rb'
 # This is little more than a coverage exercise.
 
 describe Hoodoo::Monkey::Patch::NewRelicMiddlewareAnalytics do
-  context 'InstanceExtensions' do
-    class NewRelicInstanceExtensionsTest
-      include Hoodoo::Monkey::Patch::NewRelicMiddlewareAnalytics::InstanceExtensions
+
+  class RSpecTestNRMAImplementation < Hoodoo::Services::Implementation
+    def list( context ); end
+  end
+
+  class RSpecTestNRMAInterface < Hoodoo::Services::Interface
+    interface :RSpecTestNRMA do
+      endpoint :rspec_test_nrma, RSpecTestNRMAImplementation
+    end
+  end
+
+  class RSpecTestNRMAService < Hoodoo::Services::Service
+    comprised_of RSpecTestNRMAInterface
+  end
+
+  def app
+    Rack::Builder.new do
+      use Hoodoo::Services::Middleware
+      run RSpecTestNRMAService.new
+    end
+  end
+
+  before :all do
+    $endpoint_monkey_log_inbound_request_count = 0
+
+    module NewRelic
+      module Agent
+        extend self
+        def add_custom_attributes( params )
+          $endpoint_monkey_log_inbound_request_count += 1
+        end
+      end
     end
 
-    it 'call NewRelic' do
-      Hoodoo::Monkey::Patch::NewRelicMiddlewareAnalytics::InstanceExtensions.monkey_log_inbound_request()
-    end
+    Hoodoo::Monkey.enable( extension_module: Hoodoo::Monkey::Patch::NewRelicMiddlewareAnalytics )
+  end
+
+  after :all do
+    Hoodoo::Monkey.disable( extension_module: Hoodoo::Monkey::Patch::NewRelicMiddlewareAnalytics )
+    Object.send( :remove_const, :NewRelic )
+  end
+
+  before :each do
+    expect( Hoodoo::Services::Middleware.ancestors ).to include( Hoodoo::Monkey::Patch::NewRelicMiddlewareAnalytics::InstanceExtensions )
+  end
+
+  it 'calls the NewRelic patch' do
+    get '/v1/rspec_test_nrma', nil, { 'CONTENT_TYPE' => 'application/json; charset=utf-8' }
+    expect( last_response.status ).to eq( 200 )
+
+    # We expect two log calls; secure inbound and normal inbound.
+    #
+    expect( $endpoint_monkey_log_inbound_request_count ).to eq( 2 )
   end
 end
--- spec/new_relic/agent/method_tracer.rb
+++ spec/new_relic/agent/method_tracer.rb
@@ -8,3 +8,28 @@
 #           02-Dec-2016 (ADH): Created.
 ########################################################################
 
+# Note that all of this will be defined when the test suite is starting up, but
+# during test runs, local redefinitions of NewRelic and *undefinitions* of that
+# constant will occur. The code only exists so that other "require"s will work
+# and thus provide coverage, mainly inside "newrelic_middleware_analytics.rb".
+#
+module NewRelic
+  module Agent
+    module MethodTracer
+
+      def self.included( klass )
+        klass.extend( ClassMethods )
+      end
+
+      module ClassMethods
+        module AddMethodTracer
+          def add_method_tracer( method_name, metric_name_code = nil, options = {} )
+          end
+        end
+
+        include AddMethodTracer
+      end
+
+    end
+  end
+end
