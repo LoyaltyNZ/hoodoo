bc3e1970aff1837e845a4e2618f25f1d5b1494e1
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2017-08-17 08:34:52 +1200

    Last minute bump to 1.18.0.
    This is a feature addition, not a hotfix; a version of 1.17.1 implied the latter.

--- lib/hoodoo/version.rb
+++ lib/hoodoo/version.rb
@@ -12,7 +12,7 @@ module Hoodoo
   # The Hoodoo gem version. If this changes, be sure to re-run
   # <tt>bundle install</tt> or <tt>bundle update</tt>.
   #
-  VERSION = '1.17.1'
+  VERSION = '1.18.0'
 
   # The Hoodoo gem date. If this changes, be sure to re-run
   # <tt>bundle install</tt> or <tt>bundle update</tt>.
