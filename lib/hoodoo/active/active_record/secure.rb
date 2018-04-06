7399ca94fbf5775c59d2fb3e778c037c3a794e96
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2018-04-06 15:36:15 +1200

    [WIP] [skip ci] First pass of code

--- lib/hoodoo/active/active_record/secure.rb
+++ lib/hoodoo/active/active_record/secure.rb
@@ -9,6 +9,8 @@
 #           25-Nov-2014 (ADH): Created.
 ########################################################################
 
+require 'hoodoo/active/active_record/security_helper'
+
 module Hoodoo
   module ActiveRecord
 
@@ -58,6 +60,16 @@ module Hoodoo
         model.extend( ClassMethods )
       end
 
+      # Convenience constant defining an equals-single-security-value wildcard
+      # security exemption using the String '*'.
+      #
+      OBJECT_EQLS_STAR = Hoodoo::ActiveRecord::Finder::SecurityHelper::eqls_wildcard( '*' )
+
+      # Convenience constant defining an included-in-enumerable-security-value
+      # wildcard security excemption using the String '*'.
+      #
+      ENUMERABLE_INCLUDES_STAR = Hoodoo::ActiveRecord::Finder::SecurityHelper::includes_wildcard( '*' )
+
       # Collection of class methods that get defined on an including class via
       # Hoodoo::ActiveRecord::Secure::included.
       #
@@ -69,7 +81,9 @@ module Hoodoo
         # alternative argument generator is given in the longhand form's
         # value Hash's +:using+ key.
         #
-        DEFAULT_SECURE_PROC = Proc.new { | model_class, database_column_name, session_field_value | [ { database_column_name => session_field_value } ] }
+        DEFAULT_SECURE_PROC = Proc.new do | model_class, database_column_name, session_field_value |
+          [ { database_column_name => session_field_value } ]
+        end
 
         # The core of out-of-the-box Hoodoo data access security layer.
         #
@@ -255,6 +269,9 @@ module Hoodoo
         # +using+::               See the _Advanced_ _query_ _conditions_
         #                         section later for details.
         #
+        # +exemptions+::          See the _Security_ _exemptions_ section later
+        #                         for details.
+        #
         # To help clarify the above, the following two calls to #secure_with
         # have exactly the same effect.
         #
@@ -369,6 +386,87 @@ module Hoodoo
         # +where+ via <tt>where( *returned_values )</tt> as part of the wider
         # query chain.
         #
+        # == Security exemptions
+        #
+        # Sometimes you might want a security bypass mechanism for things like
+        # a Superuser style caller that can "see everything". It's more secure,
+        # where possible and scalable, to simply have the session data match
+        # every known value of some particular secured-with quantity, but this
+        # might get unwieldy. "WHERE IN" queries with hundreds or thousands of
+        # listed items can cause problems!
+        #
+        # Noting that with any security exemption there is elevated risk, you
+        # can use the +:exemptions+ key to provide a Proc which is passed the
+        # secure value(s) under consideration (the data taken directly from
+        # the session scoping section and evaluates to +true+ if the value(s)
+        # indicate that a security exemption applies, else evaluates "falsey"
+        # for normal behaviour. We say "value(s)" here as a single key yielding
+        # data from the scoping section of a session may yield either a simple
+        # value such as a String, or an Enumerable object such as an array of
+        # many Strings.
+        #
+        # If the Proc evaluates to +true+, the result is no modification to the
+        # secure scope chain being constructed for the secured ActiveRecord
+        # query the caller will eventually run. Helper methods which construct
+        # common use case Procs are present in
+        # Hoodoo::ActiveRecord::Finder::SecurityHelper and there are
+        # convenience constants defined in Hoodoo::ActiveRecord::Secure, such
+        # as Hoodoo::ActiveRecord::Secure::ENUMERABLE_INCLUDES_STAR.
+        #
+        # Taking an earlier example:
+        #
+        #     secure_with( {
+        #       :creating_caller_uuid => :authorised_caller_uuids
+        #     } )
+        #
+        #     # ...has this minimal longhand equivalent...
+        #
+        #     secure_with( {
+        #       :creating_caller_uuid => {
+        #         :session_field_name => :authorised_caller_uuids
+        #       }
+        #     } )
+        #
+        # ...which leads to SQL along the following lines:
+        #
+        #    AND ("model_table"."creating_caller_uuid" = '[val]')
+        #
+        # ...then suppose we wanted to allow a session scoping value of '*' to
+        # bypass security ("see everythign"). We could use the single-object
+        # star matcher Proc in Hoodoo::ActiveRecord::Secure::OBJECT_EQLS_STAR
+        # here. At the time of writing, it is defined as the following Proc:
+        #
+        #    Proc.new do | security_value |
+        #      security_value.eql?( '*' )
+        #    end
+        #
+        # This is activated through the +:exemptions+ key:
+        #
+        #     secure_with( {
+        #       :creating_caller_uuid => {
+        #         :session_field_name => :authorised_caller_uuids,
+        #         :exemptions         => Hoodoo::ActiveRecord::Secure::OBJECT_EQLS_STAR
+        #       }
+        #     } )
+        #
+        # Now, if the looked up value of the +authorised_caller_uuid+ attribute
+        # in the prevailing Session scoping section data was "1234" then the
+        # SQL query additions would occur as above:
+        #
+        #    AND ("model_table"."creating_caller_uuid" = '1234')
+        #
+        # ...but if there is a value of "*", the security layer will ignore the
+        # normal restrictions, resulting in no SQL additions whatsoever.
+        #
+        # Since a Proc is used to compare the data found in the session against
+        # some wildcard, things like checking an array of values for some magic
+        # bypass characters / key, using regular expression matching, or other
+        # more heavyweight options are all possible. Remember, though, that all
+        # of this comes at a risk, since the mechanism is bypassing the normal
+        # scope chain security. If used improperly or somehow compromised, it
+        # will allow data to be read by an API caller that should not have been
+        # permitted to access it.
+        #
         def secure( context )
           prevailing_scope = all() # "Model.all" -> returns anonymous scope
           extra_scope_map  = secured_with()
@@ -377,25 +475,33 @@ module Hoodoo
             return none() if context.session.nil? || context.session.scoping.nil?
 
             extra_scope_map.each do | model_field_name, key_or_options |
-              params_proc = DEFAULT_SECURE_PROC
+              exemption_proc = nil
+              params_proc    = DEFAULT_SECURE_PROC
 
               if key_or_options.is_a?( Hash )
                 session_scoping_key = key_or_options[ :session_field_name ]
+                exemption_proc      = key_or_options[ :exemptions ]
                 params_proc         = key_or_options[ :using ] if key_or_options.has_key?( :using )
               else
                 session_scoping_key = key_or_options
               end
 
               if context.session.scoping.respond_to?( session_scoping_key )
-                args = params_proc.call(
-                  self,
-                  model_field_name,
-                  context.session.scoping.send( session_scoping_key )
-                )
-                prevailing_scope = prevailing_scope.where( *args )
+                security_value = context.session.scoping.send( session_scoping_key )
+
+                if exemption_proc.nil? || exemption_proc.call( security_value ) != true
+                  args = params_proc.call(
+                    self,
+                    model_field_name,
+                    security_value
+                  )
+                  prevailing_scope = prevailing_scope.where( *args )
+                end
+
               else
                 prevailing_scope = none()
                 break
+
               end
             end
           end
---
+++ lib/hoodoo/active/active_record/security_helper.rb
@@ -0,0 +1,113 @@
+########################################################################
+# File::    security_helper.rb
+# (C)::     Loyalty New Zealand 2018
+#
+# Purpose:: Supplementary helper class included by "finder.rb". See
+#           Hoodoo::ActiveRecord::Secure, especially
+#           Hoodoo::ActiveRecord::Secure::ClassMethods#secure_with and
+#           its options Hash, for details.
+# ----------------------------------------------------------------------
+#           05-Apr-2018 (ADH): Created.
+########################################################################
+
+module Hoodoo
+  module ActiveRecord
+    module Finder
+
+      # Help build security exemption Procs to pass into
+      # Hoodoo::ActiveRecord::Secure::ClassMethods#secure_with via its options
+      # Hash. The following extends an example given in the documentation (at
+      # the time of writing here) for the underlying implementation method
+      # Hoodoo::ActiveRecord::Secure::ClassMethods#secure:
+      #
+      #     class Audit < ActiveRecord::Base
+      #       include Hoodoo::ActiveRecord::Secure
+      #
+      #       secure_with(
+      #         {
+      #           :creating_caller_uuid => :authorised_caller_uuids
+      #         },
+      #         {
+      #           :exemptions => Hoodoo::ActiveRecord::Finder::SecurityHelper::includes_wildcard( '*' )
+      #         }
+      #       )
+      #     end
+      #
+      # Note that the Hoodoo::ActiveRecord::Secure module includes some belper
+      # constants to aid brevity for common cases such as the single value
+      # <tt>#eql?</tt> or enumerable <tt>#include?</tt> matchers checking for
+      # a '*' as an indiscriminate wildcard - see for example
+      # Hoodoo::ActiveRecord::Secure::ENUMERABLE_INCLUDES_STAR.
+      #
+      class SecurityHelper
+
+        # Match a given wildcard, typically a String, to a single value
+        # via <tt>#eql?</tt>.
+        #
+        # +wildcard_value+:: Wildcard value to match, e.g. <tt>'*'</tt>.
+        #
+        # Returns a Proc suitable for passing to the +:exemptions+ option for
+        # Hoodoo::ActiveRecord::Secure::ClassMethods#secure_with.
+        #
+        def self.eqls_wildcard( wildcard_value )
+          Proc.new do | security_value |
+            security_value.eql?( wildcard_value )
+          end
+        end
+
+        # Match a given wildcard, typically a String, inside an Enumerable
+        # subclass via <tt>#include?</tt>.
+        #
+        # +wildcard_value+:: Wildcard value to match, e.g. <tt>'*'</tt>.
+        #
+        # Returns a Proc suitable for passing to the +:exemptions+ option for
+        # Hoodoo::ActiveRecord::Secure::ClassMethods#secure_with.
+        #
+        def self.includes_wildcard( wildcard_value )
+          Proc.new do | security_values |
+            security_values.include?( wildcard_value )
+          end
+        end
+
+        # Match a given wildcard Regexp to a value via <tt>#match?</tt>.
+        #
+        # +wildcard_value+:: Wildcard Regexp to use, e.g. <tt>/.*/</tt>.
+        #                    Strings are coerced to Regexps without any
+        #                    escaping but doing so reduces performance.
+        #
+        # Returns a Proc suitable for passing to the +:exemptions+ option for
+        # Hoodoo::ActiveRecord::Secure::ClassMethods#secure_with.
+        #
+        def self.wildcard_matches( wildcard_regexp )
+          wildcard_regexp = Regexp.new( wildcard_regexp ) unless wildcard_regexp.is_a?( Regexp )
+
+          Proc.new do | security_value |
+            wildcard_regexp.match?( security_value )
+          end
+        end
+
+        # Match a given wildcard Regexp to any value in an enumerable
+        # object via iteration and <tt>#match?</tt>. Exists with +true+
+        # as soon as any match is made.
+        #
+        # +wildcard_value+:: Wildcard Regexp to use, e.g. <tt>/.*/</tt>.
+        #                    Strings are coerced to Regexps without any
+        #                    escaping but doing so reduces performance.
+        #
+        # Returns a Proc suitable for passing to the +:exemptions+ option for
+        # Hoodoo::ActiveRecord::Secure::ClassMethods#secure_with.
+        #
+        def self.wildcard_matches_enumerable( wildcard_regexp )
+          wildcard_regexp = Regexp.new( wildcard_regexp ) unless wildcard_regexp.is_a?( Regexp )
+
+          Proc.new do | security_values |
+            security_values.each do | security_value |
+              return true if wildcard_regexp.match?( security_value )
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
