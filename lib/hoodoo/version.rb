d012d643a8044afa44832dc1678ecf32f3c5a586
Minh Ha <minh.ha@loyalty.co.nz>
2021-09-28 23:41:41 +1300

    FT-536 2.12.10, os: linux

--- lib/hoodoo/version.rb
+++ lib/hoodoo/version.rb
@@ -12,7 +12,7 @@ module Hoodoo
   # The Hoodoo gem version. If this changes, be sure to re-run
   # <tt>bundle install</tt> or <tt>bundle update</tt>.
   #
-  VERSION = '2.12.10.pre'
+  VERSION = '2.12.10'
 
   # The Hoodoo gem date. If this changes, be sure to re-run
   # <tt>bundle install</tt> or <tt>bundle update</tt>.
